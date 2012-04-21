40dca107f77746be06474e8ab8a94fc1f43ce0b9
Steve Faulkner <southpolesteve@gmail.com>
2012-04-21 12:58:02 -0400

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipster::Application.config.secret_token = 'e3f004ea2caca8c62206bf46bda73d1083020eb82aa7575af06907df1431087fe18db99468067efc81f29cf1243a60d07950e886666c860cd773ee94311ff1a1'
