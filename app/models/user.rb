008386f7ff5fc47d610342bb6e59a847f6aef5da
Steve Faulkner <southpolesteve@gmail.com>
2012-04-21 15:59:54 -0400

    Add Facebook auth methods on create and necessary fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User
   field :uid, :type => String
   field :name, :type => String
   field :email, :type => String
-  attr_accessible :provider, :uid, :name, :email
+  field :token, :type => String
+  attr_accessible :provider, :uid, :name, :email, :token
 
   def self.create_with_omniauth(auth)
     create! do |user|
@@ -14,7 +15,14 @@ class User
          user.name = auth['info']['name'] || ""
          user.email = auth['info']['email'] || ""
       end
+      if auth['credentials']
+        user.token = auth['credentials']['token']
+      end
     end
+  end
+
+  def facebook
+    Koala::Facebook::API.new(token)
   end
 
 end
