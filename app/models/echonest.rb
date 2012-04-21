63f26cb27086c9af48628438a634fce716aebd09
Steve Faulkner <southpolesteve@gmail.com>
2012-04-21 16:00:39 -0400

    Add model for talking to Echonest

---
+++ app/models/echonest.rb
@@ -0,0 +1,24 @@
+class Echonest
+  include HTTParty
+  base_uri "http://developer.echonest.com/api/v4"
+  default_params :api_key => "ABUMALOOTAJPCMUKU"
+
+  def self.hottnesss(artist_name)
+    response = get("/artist/hotttnesss/", :query => {:name => artist_name })['response']
+    if response['artist']
+      response['artist']['hotttnesss']
+    else
+      nil
+    end
+  end
+
+  def self.familiarity(artist_name)
+    response = get("/artist/familiarity/", :query => {:name => artist_name })['response']
+    if response['artist']
+      response['artist']['familiarity']
+    else
+      nil
+    end
+  end
+
+end
